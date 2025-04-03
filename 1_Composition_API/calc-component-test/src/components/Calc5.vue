758f5bb012be25e9251d63976b472b9ef3e55fae
dahyun-812 <dah9902@naver.com>
2025-04-03 14:52:33 +0900

    250402 - 1차 실습 과제

---
+++ 1_Composition_API/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,29 @@
+<script>
+import { ref, watch } from 'vue'
+
+export default {
+  name: 'Calc5',
+  setup() {
+    const x = ref(0) // 0으로 초기화
+
+    const result = ref(0) // 0으로 초기화
+    // 필요한 경우 코드 추가
+
+    watch(x, (current, old) => {
+      console.log(`${old}에서 ${current}로`)
+      result.value = current
+    })
+
+    return { x, result }
+  },
+}
+</script>
+
+<template>
+  <div>
+    X : <input type="text" v-model.number="x" /> <br />
+    결과 : {{ result }}
+  </div>
+</template>
+
+<style scoped></style>
