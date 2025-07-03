73365889f13c0ed05e0885fa0341e4ee8cec81a3
dahyun-812 <dah9902@naver.com>
2025-07-03 17:46:16 +0900

    심화 - 01. 프론트엔드 준비

---
+++ 09_Spring-Vue/ch01-project-start/frontend/src/App.vue
@@ -0,0 +1,78 @@
+<!-- <script setup>은 Vue3에서 Composition API를 더 간단하게 쓸 수 있게 해주는 문법 -->
+<script setup>
+// RouterView는 vue-router에서 제공하는 컴포넌트
+// 현재 URL 경로에 따라 맞는 컴포넌트를 화면에 보여줌
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<!-- 화면에 보여줄 HTML 구조 정의하는 부분 -->
+<template>
+  <!-- RouterView는 라우터 설정에 따라 해당 컴포넌트를 이 위치에 표시함 -->
+  <RouterView />
+</template>
+
+<!-- scoped는 이 CSS가 이 컴포넌트에만 적용되게 제한 -->
+<!-- scoped가 없으면 다른 컴포넌트를 합했을 때 모두 영향을 미치게 됨. -> 전역 설정 -->
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ 09_Spring-Vue/ch01-project-start/frontend/src/components/lauouts/NavBar.vue
@@ -0,0 +1,10 @@
+<script setup>
+// Vue의 Composition API에서 반응형 객체와 속성을 불러옴
+import { reactive, computed } from 'vue'
+</script>
+
+<template>
+  <div class="NavBar"></div>
+</template>
+
+<style scoped></style>
